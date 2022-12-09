57bd3fdb596fa039a3be3bcf443ea3f368662131
Crash <101067257+Crashino@users.noreply.github.com>
2022-12-09 10:56:31 +0100

    Update b.lua

--- b.lua
+++ b.lua
@@ -1,1 +1,3 @@
-print("cum")
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+
+local Window = Library.CreateLib("Krypton X-v0.79", "RJTheme7")
